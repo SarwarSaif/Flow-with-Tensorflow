8c378ebc7f65bf9938dec51d4f5d5355b5c6690e
Sarwar Saif <saifmu6@gmail.com>
2019-11-15 02:25:14 +0600

    Created a Keras Model

--- initial_test.py
+++ initial_test.py
@@ -14,5 +14,24 @@ train_images = train_images/255.0
 test_images = test_images/255.0
 
 ## Show Dataas Image in Matplotlib
-plt.imshow(train_images[7], cmap = plt.cm.binary)
-plt.show()
+##plt.imshow(train_images[7], cmap = plt.cm.binary)
+##plt.show()
+
+## Create Model with keras
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="sigmoid"),
+    keras.layers.Dense(64, activation="relu"),
+    keras.layers.Dense(32, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=15)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Accuracy: ", test_acc)
+
+
